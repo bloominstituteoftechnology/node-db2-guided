8fa6777e66d4cc952a09002843f6b95acf6b4865
Diandra Ryan-Mas <dryanmas@Diandras-MacBook-Air.local>
2019-07-13 07:28:28 -0700

    knex hardcoded in

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const knex = require('knex');
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './data/produce.db3'
+  },
+  useNullAsDefault: true
+});
 
 const router = express.Router();
 
