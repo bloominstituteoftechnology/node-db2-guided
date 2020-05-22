4c32f98e958f56e302ae011d0234b690cb1d384a
Veto Ramirez <vetorobert3@gmail.com>
2020-05-21 19:44:29 -0700

    Created helper file knexfile for the fruits router.

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+
+    connection: {
+      filename: './data/produce-2.db3'
+    },
+
+   useNullAsDefault: true
+  }
+
+};
