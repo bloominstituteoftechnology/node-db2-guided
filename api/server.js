765a70b7de88436829043e2b187728d2fb1c271d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-10-05 11:25:39 -0400

    upgrade deps

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const helmet = require('helmet');
 
 const fruitsRouter = require('./fruits/fruits-router.js');
 
 const server = express();
 
-server.use(helmet());
 server.use(express.json());
 
 server.use('/api/fruits', fruitsRouter);
