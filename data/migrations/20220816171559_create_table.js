1b37bd046455c6f2860c00adf8655bae1e26ee9c
James Blacklock <jamesblacklock@protonmail.com>
2022-08-16 13:17:03 -0400

    committing to "lecture"

--- data/migrations/20220816171559_create_table.js
+++ data/migrations/20220816171559_create_table.js
@@ -1,15 +1,8 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.up = function(knex) {
   
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
   
 };
