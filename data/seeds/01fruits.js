5b38581c0e0291d64710528af2a784ce161a43f9
James Blacklock <jamesblacklock@protonmail.com>
2022-08-16 13:55:53 -0400

    committing to "lecture"

--- data/seeds/01fruits.js
+++ data/seeds/01fruits.js
@@ -1,7 +1,7 @@
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('fruits').del()
+  await knex('fruits').truncate()
   await knex('fruits').insert([
     { name: 'apple', color: 'red', delicious: true, avgWeightOz: 10},
     { name: 'banana', color: 'yellow', delicious: true, avgWeightOz: 10},
